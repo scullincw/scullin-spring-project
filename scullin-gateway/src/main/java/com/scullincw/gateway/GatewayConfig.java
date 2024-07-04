c81eb504a856d6f864e588e30d65bd02fe2ab57c
scullincw <scullincw@gmail.com>
2024-07-04 11:51:33 +0800

    scullin-gateway

---
+++ scullin-gateway/src/main/java/com/scullincw/gateway/GatewayConfig.java
@@ -0,0 +1,26 @@
+package com.scullincw.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // 这里的配置会覆盖application.yml中的
+                .route("path_route", r -> r.path("/get")
+                        .filters(f -> f.addRequestHeader("X-Scullin-Request", "blue"))
+                        .uri("http://httpbin.org"))
+
+                .route("host_route", r -> r.host("*.myhost.org")
+                        .uri("http://httpbin.org"))
+                .route("rewrite_route", r -> r.host("*.rewrite.org")
+                        .filters(f -> f.rewritePath("/foo/(?<segment>.*)", "/${segment}"))
+                        .uri("http://httpbin.org"))
+                .build();
+    }
+}
